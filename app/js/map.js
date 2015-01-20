407248a337bace5adfa1c1d31735327c99d25752
Miguel Salazar <miguelsalazarg@gmail.com>
2015-01-20 01:22:52 -0600

    Sidebar interactions

--- app/js/map.js
+++ app/js/map.js
@@ -1,13 +1,13 @@
 // Canvas size
 var width = 560,
-	height = 600,
+	height = 560,
 	active = d3.select(null);
 
 // Map properties
 var projection = d3.geo.mercator()
-	.scale(318)
+	.scale(300)
 	.translate([width / 2, 280])
-	.center([-76,-13])
+	.center([-76,-16])
 	.precision(0);
 
 var path = d3.geo.path()
@@ -27,6 +27,11 @@ var g = svg.append("g")
 	.style("stroke-width", ".5px");
 
 var compareFlag = false;
+var cityView = false;
+var current;
+
+var dropDown = d3.selectAll(".map").append("select").attr("name", "country-list");
+
 
 d3.json("../data/la.json", function(error, la) {
 	var countries = topojson.feature(la, la.objects.countries);
@@ -70,13 +75,15 @@ d3.json("../data/la.json", function(error, la) {
 		.attr("d", path)
 		.attr("visibility", "hidden")
 		.on("mouseover", function(d) {
-		});
+		})
+		.on("click", onCityClick);
 
 	g.append("path")
 		.datum(cities)
 		.attr("d", path)
 		.attr("class", "city")
-		.attr("visibility", "hidden");
+		.attr("visibility", "hidden")
+		.on("click", onCityClick);
 
 	g.selectAll(".city-label")
 		.data(cities.features)
@@ -93,7 +100,81 @@ d3.json("../data/la.json", function(error, la) {
 	
 });
 
+function showCities() {
+	d3.selectAll(".city").attr("visibility","visible");
+	d3.selectAll(".city-label").attr("visibility","visible");
+
+	d3.selectAll(".city")
+		.on("mouseover", function(d) {
+			d3.select(this).attr("class", ".city-hover");
+		})
+		.on("mouseout", function(d) {
+			d3.select(this).attr("class", ".city");
+		});
+
+	d3.selectAll(".city-label")
+		.text(function(d) {
+			return d.properties.name; 
+		})
+		.on("mouseover", function(d) {
+			var nodeSelection = d3.select(this).style("fill", "#9C5308");
+			nodeSelection.select("text").style("fill", "#9C5308");
+		})
+		.on("mouseout", function(d) {
+			var nodeSelection = d3.select(this).style("fill", "#444");
+			nodeSelection.select("text").style("fill", "#444");
+		});
+}
+
+function hideCities() {
+	d3.selectAll(".city").attr("visibility","hidden");
+	d3.selectAll(".city-label").attr("visibility","hidden");
+}
+
+function showCountryLabels() {
+	d3.selectAll(".country-label")
+		.style("visibility", "visible");
+}
+
+function hideCountryLabels() {
+	d3.selectAll(".country-label")
+		.style("visibility", "hidden");
+}
+
+function showLeftPanel() {
+	document.getElementById('panel-left').style.visibility="visible";
+}
+
+function hideLeftPanel() {
+	document.getElementById('panel-left').style.visibility="hidden";
+}
+
+function showRightPanel() {
+	document.getElementById('panel-right').style.visibility="visible";
+}
+
+function hideRightPanel() {
+	document.getElementById('panel-right').style.visibility="hidden";
+}
+
+function closeLeftPanel() {
+	document.getElementById('panel-left').style.visibility="hidden";
+}
+
+function closeRightPanel() {
+	document.getElementById('panel-right').style.visibility="hidden";
+}
+
 function onCountryClick(d) {
+	current = document.getElementById('current').innerHTML;
+	//var current_chart = "";
+	//current_chart = current_chart.concat("chart",current,"a");
+	//console.log(current_chart);
+	//document.getElemendById(current_chart).style.display = "none";
+	//document.getElementById('chart1a').style.display = "block";
+	document.getElementById('current').innerHTML = 1;
+	document.getElementById('total').innerHTML = 4;
+
 	small = ["COR", "DOM", "ECU", "GUA", "HON",  "NIC", "PAN", "PAR", "URU"];
 	large = ["ARG", "BRA", "CHI", "MEX"];
 
@@ -119,45 +200,20 @@ function onCountryClick(d) {
         	d3.select(this).attr("class", function(d) { return "country " + d.properties.category; });
 	});
 
-	g.selectAll(".city")
-		.on("mouseover", function(d) {
-			d3.select(this).attr("class", ".city-hover");
-		})
-		.on("mouseout", function(d) {
-			d3.select(this).attr("class", ".city");
-		});
-
-	g.selectAll(".city-label")
-		.text(function(d) {
-			return d.properties.name; 
-		})
-		.on("mouseover", function(d) {
-			var nodeSelection = d3.select(this).style("fill", "#9C5308");
-			nodeSelection.select("text").style("fill", "#9C5308");
-		})
-		.on("mouseout", function(d) {
-			var nodeSelection = d3.select(this).style("fill", "#444");
-			nodeSelection.select("text").style("fill", "#444");
-		});
-
+	hideCountryLabels();
+	showCities();
 
 	if(compareFlag == false) {
 		if (active.node() === this) {
 			document.getElementById('entity-a').innerHTML='';
-			//document.getElementById('gini').innerHTML='';
-			document.getElementById('panel-left').style.visibility="hidden";
-			d3.selectAll(".city").attr("visibility","hidden");
-			d3.selectAll(".city-label").attr("visibility","hidden");
+			hideLeftPanel();
 			return reset();
 		}
 
-		document.getElementById('panel-left').style.visibility="visible";
+		showLeftPanel();
 
 		active.classed("active", false);
 		active = d3.select(this).classed("active", true);
-		d3.selectAll(".city").attr("visibility","visible");
-		d3.selectAll(".country-label").attr("visibility","hidden");
-		d3.selectAll(".city-label").attr("visibility","visible");
 		
 		var bounds = path.bounds(d),
 			dx = bounds[1][0] - bounds[0][0],
@@ -172,24 +228,22 @@ function onCountryClick(d) {
 			.attr("transform", "translate(" + translate + ")scale(" + scale + ")");
 		var country = d.properties.name;
 		document.getElementById('entity-a').innerHTML=country;
-		//var gini = d.properties.gini;
-		//document.getElementById('gini').innerHTML=gini;
 	} 
 
 	else if (compareFlag == true) {
 		if (active.node() === this) {
 			document.getElementById('entity-b').innerHTML='';
-			//document.getElementById('gini').innerHTML='';
-			document.getElementById('panel-right').style.visibility="hidden";
-			d3.selectAll(".city-label").attr("visibility","hidden");
+			hideRightPanel();
+			hideCities();
 			return reset();
 		}
-		document.getElementById('panel-right').style.visibility="visible";
 
+		showRightPanel();
 		active.classed("active", false);
 		active = d3.select(this).classed("active", true);
-		d3.selectAll(".city-label").attr("visibility","visible");
-			var bounds = path.bounds(d),
+		showCities();
+		
+		var bounds = path.bounds(d),
 			dx = bounds[1][0] - bounds[0][0],
 			dy = bounds[1][1] - bounds[0][1],
 			x = (bounds[0][0] + bounds[1][0]) / 2,
@@ -203,175 +257,80 @@ function onCountryClick(d) {
 		var country = d.properties.name;
 		document.getElementById('entity-b').innerHTML=country;
 		toggleCompareFlag();
-		//var gini = d.properties.gini;
-		//document.getElementById('gini').innerHTML=gini;
 	}
-	//graphGiniCountry();
 }
 
-
 function onCityClick(d) {
-	document.getElementById('entity-a').innerHTML='';
-	document.getElementById('title-1a').innerHTML='';
-	document.getElementById('chart-1a').innerHTML='';
-	document.getElementById('title-2a').innerHTML='';
-	document.getElementById('chart-2a').innerHTML='';
-	document.getElementById('title-3a').innerHTML='';
-	document.getElementById('chart-3a').innerHTML='';
-	
+
 	var city_name = d.properties.name;
-	document.getElementById('entity-a').innerHTML= city_name;
-	document.getElementById('title-1a').innerHTML='Índice de Gini';
-	document.getElementById('title-2a').innerHTML='Ingreso per cápita';
-	document.getElementById('title-3a').innerHTML='Distribución de ingreso';
-	
-	var country_code = d.properties.country;
-	country_code = country_code.toLowerCase();
-	var giniDataURL = "../data/" + country_code + "/" + country_code + "_gini.csv";
-	var ipcDataURL = "../data/" + country_code + "/" + country_code + "_ipc.csv";
 
-	d3.csv(giniDataURL, function(error, csv) {
-		var years = d3.keys(csv[0]);
-		years.splice(years.indexOf("city"));
-		years.sort();
+	if(compareFlag) {
+		document.getElementById('entity-b').innerHTML= city_name;
+	} else {
+		document.getElementById('entity-a').innerHTML= city_name;
+		cityComparisonFlag = true;
+	}
+}
 
-		var year;
-		var gini;
-		var giniCityData = [];
+function next() {
+	current = document.getElementById('current').innerHTML;
+	total = document.getElementById('total').innerHTML;
 
-        csv = csv.filter(function(row) {
-        	return row['city'] == city_name;
-    	});
+	if(current < total) {
+		current++;
+		current = parseInt(document.getElementById('current').innerHTML = current);
+	}
+	switch(current) {
+		case 2:
+			document.getElementById('chart1a').style.display = "none";
+			document.getElementById('chart2a').style.display = "block";
+			break;
+		case 3:
+			document.getElementById('chart2a').style.display = "none";
+			document.getElementById('chart3a').style.display = "block";
+			break;
+		case 4:
+			document.getElementById('chart3a').style.display = "none";
+			document.getElementById('chart4a').style.display = "block";
+			break;
+		default:
+			break;
+	}
+}
 
-    	if(years[0]) {
-    		year = years[0];
-    		gini = csv[0][year];
-    		if(gini) {
-    			giniData = { year: year, gini: gini, city: city_name };
-    			giniCityData.push(giniData);
-    		}
-    	}
-		if(years[1]) {
-			year = years[1];
-    		gini = csv[0][year];
-    		if(gini) {
-    			giniData = { year: year, gini: gini, city: city_name };
-    			giniCityData.push(giniData);
-    		}
-		}
-		if(years[2]) {
-			year = years[2];
-    		gini = csv[0][year];
-    		if(gini) {
-    			giniData = { year: year, gini: gini, city: city_name };
-    			giniCityData.push(giniData);
-    		}
-		}
-		if(years[3]) {
-			year = years[3];
-    		gini = csv[0][year];
-    		if(gini) {
-    			giniData = { year: year, gini: gini, city: city_name };
-    			giniCityData.push(giniData);
-    		}
-		}
-		if(years[4]) {
-			year = years[4];
-    		gini = csv[0][year];
-    		if(gini) {
-    			giniData = { year: year, gini: gini, city: city_name };
-    			giniCityData.push(giniData);
-    		}
-		}
-		if(years[5]) {
-			year = years[5];
-    		gini = csv[0][year];
-    		if(gini) {
-    			giniData = { year: year, gini: gini, city: city_name };
-    			giniCityData.push(giniData);
-    		}
-		}
-
-    	graphGiniCity(giniCityData);
-
-	});
-
-	d3.csv(ipcDataURL, function(error, csv) {
-
-		var years = d3.keys(csv[0]);
-		years.splice(years.indexOf("city"));
-		years.sort();
-
-		var year;
-		var ipc;
-		var ipcCityData = [];
-
-        csv = csv.filter(function(row) {
-        	return row['city'] == city_name;
-    	});
-
-    	if(years[0]) {
-    		year = years[0];
-    		ipc = csv[0][year];
-    		if(ipc) {
-    			ipcData = { year: year, ipc: ipc };
-    			ipcCityData.push(ipcData);
-    		}
-    	}
-		if(years[1]) {
-			year = years[1];
-    		ipc = csv[0][year];
-    		if(ipc) {
-    			ipcData = { year: year, ipc: ipc };
-    			ipcCityData.push(ipcData);
-    		}
-		}
-		if(years[2]) {
-			year = years[2];
-    		ipc = csv[0][year];
-    		if(ipc) {
-    			ipcData = { year: year, ipc: ipc };
-    			ipcCityData.push(ipcData);
-    		}
-		}
-		if(years[3]) {
-			year = years[3];
-    		ipc = csv[0][year];
-    		if(ipc) {
-    			ipcData = { year: year, ipc: ipc };
-    			ipcCityData.push(ipcData);
-    		}
-		}
-		if(years[4]) {
-			year = years[4];
-    		ipc = csv[0][year];
-    		if(ipc) {
-    			ipcData = { year: year, ipc: ipc };
-    			ipcCityData.push(ipcData);
-    		}
-		}
-		if(years[5]) {
-			year = years[5];
-    		ipc = csv[0][year];
-    		if(ipc) {
-    			ipcData = { year: year, ipc: ipc };
-    			ipcCityData.push(ipcData);
-    		}
-		}
-
-    	graphIpcCity(ipcCityData);		
-	});
-
+function prev() {
+	if(current > 1) {
+		current--;
+		current = parseInt(document.getElementById('current').innerHTML = current);
+	}
+	switch(current) {
+		case 1:
+			document.getElementById('chart2a').style.display = "none";
+			document.getElementById('chart1a').style.display = "block";
+			break;
+		case 2:
+			document.getElementById('chart3a').style.display = "none";
+			document.getElementById('chart2a').style.display = "block";
+			break;
+		case 3:
+			document.getElementById('chart4a').style.display = "none";
+			document.getElementById('chart3a').style.display = "block";
+			break;
+		default:
+			break;
+	}
 }
 
 function reset() {
+	hideCities();
+
 	if(compareFlag == false) {
-		document.getElementById('panel-left').style.visibility="hidden";
-		document.getElementById('panel-right').style.visibility="hidden";
-		document.getElementById('entity-a').innerHTML="";
+		//hideLeftPanel();
+		//hideRightPanel();
+		//document.getElementById('entity-a').innerHTML="";
 	} 
 	else if(compareFlag == true) {
-		document.getElementById('panel-right').style.visibility="hidden";
+		//hideRightPanel();
 	}
 
 	active.classed("active", false);
@@ -387,15 +346,8 @@ function reset() {
         	d3.select(this).attr("class", "country-hover");
 	});
 
-	g.selectAll(".city")
-		.style("visibility", "hidden");
-
-	g.selectAll(".city-label")
-		.style("visibility", "hidden");
-
-	g.selectAll(".country-label")
-		.style("visibility", "visible");
-
+	hideCities();
+	showCountryLabels();
 }
 
 function toggleCompareFlag() {
@@ -405,209 +357,6 @@ function toggleCompareFlag() {
 
 function compare() {
 	toggleCompareFlag();
-}
-
-/* Graphing Functions */
-function graphGiniCity(data) {
-
-	var	margin = {top: 5, right: 5, bottom: 5, left: 5},
-	width = 220 - margin.left - margin.right,
-	height = 140 - margin.top - margin.bottom;
- 
-	// Parse the date / time
-	var	parseDate = d3.time.format("%Y").parse;
-
-	// Set the ranges
-	var	x = d3.time.scale().range([0, width]);
-	var	y = d3.scale.linear().range([height, 0]);
-	 
-	// Define the axes
-	var	xAxis = d3.svg.axis().scale(x)
-		.orient("bottom");
-	 
-	var	yAxis = d3.svg.axis().scale(y)
-		.orient("left").ticks(6);
-
-	// Define the line
-	var	valueline = d3.svg.line()
-		.x(function(d) { return x(d.date); })
-		.y(function(d) { return y(d.gini); });
-
-	// Adds the svg canvas
-	var	svg = d3.select("chart-1a")
-		.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	// Get the data
-	data.forEach(function(d) {
-		d.year = parseDate(d.year);
-		d.gini = +d.gini;
-	});
-	 
-	// Scale the range of the data
-	x.domain(d3.extent(data, function(d) { return d.year; }));
-	y.domain([0, d3.max(data, function(d) { return d.gini; })]);
-
-	// Add the valueline path.
-	svg.append("linechartpath")	
-		.attr("class", "linechartline")
-		.attr("d", valueline(data));
-
-	xAxis.tickValues(data.map(function(d){return d.year;}));
-
-	// Add the X Axis
-	svg.append("g")		
-		.attr("class", "x axis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis);
-
-	// Add the Y Axis
-	svg.append("g")		
-		.attr("class", "y axis")
-		.call(yAxis);
-
-	svg.select('linechartline').style({ 'stroke-width': '0px'});
-
-	var dataCirclesGroup = svg.append('svg:g');
-
-	var tooltip = d3.select("body")
-    	.append("div")
-    	.attr("class", "tooltip")
-		.style("opacity", 0);
-
-	var circles = dataCirclesGroup.selectAll('.data-point')
-				.data(data);
-
-	circles
-		.enter()
-		.append('svg:circle')
-		
-		.attr('class', 'dot')
-		.attr('fill', function() { return "steelblue"; })
-		.attr('cx', function(d) { return x(d["year"]); })
-		.attr('cy', function(d) { return y(d["gini"]); })
-		.attr('r', function() { return 3; })
-		.on("mouseover", function(d) {
-			d3.select(this)
-				.attr("r", 8)
-				.attr("class", "dot-selected")
-				.transition()
-  				.duration(750);
-  			d3.select(this)
-  				.append('svg:title')
-  				.text(function(d) {
-					return d.gini;
-				});
-			return tooltip.style("visibility", "visible");	
-		})
-		.on("mouseout", function(d) {
-				d3.select(this)
-				.attr("r", 3)
-				.attr("class", "dot")
-				.transition()
-  					.duration(750);
-		});
-}
-
-function graphIpcCity(ipcCityData) {
-
-}
-
-function graphGiniCountry() {
-	var	margin = {top: 0, right: 0, bottom: 0, left: 0},
-	width = 210 - margin.left - margin.right,
-	height = 120 - margin.top - margin.bottom;
- 
-	// Parse the date / time
-	var	parseDate = d3.time.format("%y").parse;
-	 
-	// Set the ranges
-	var	x = d3.time.scale().range([0, width]);
-	var	y = d3.scale.linear().range([height, 0]);
-	 
-	// Define the axes
-	var	xAxis = d3.svg.axis().scale(x)
-		.orient("bottom");
-	 
-	var	yAxis = d3.svg.axis().scale(y)
-		.orient("left").ticks(6)
-
-	// Define the line
-	var	valueline = d3.svg.line()
-		.x(function(d) { return x(d.date); })
-		.y(function(d) { return y(d.close); });
-
-	// Adds the svg canvas
-	var	svg = d3.select("#chart-1a")
-		.append("svg")
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	// Get the data
-	d3.csv("/components/lineagini/data.csv", function(error, data) {
-		data.forEach(function(d) {
-			d.date = parseDate(d.date);
-			d.close = +d.close;
-		});
-	 
-		// Scale the range of the data
-		x.domain(d3.extent(data, function(d) { return d.date; }));
-		y.domain([0, d3.max(data, function(d) { return d.close; })]);
-
-		// Add the valueline path.
-		svg.append("gini-path")	
-			.attr("class", "gini-line")
-			.attr("d", valueline(data));
-
-		xAxis.tickValues(data.map(function(d){return d.date;}));
-
-		// Add the X Axis
-		svg.append("g")		
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
-			.call(xAxis);
-
-		// Add the Y Axis
-		svg.append("g")		
-			.attr("class", "y axis")
-			.call(yAxis);
-
-		svg.select('gini-line').style({ 'stroke-width': '0px'});
-
-		var dataCirclesGroup = svg.append('svg:g');
-
-		var circles = dataCirclesGroup.selectAll('.data-point')
-					.data(data);
-
-				circles
-					.enter()
-					.append('svg:circle')
-					.attr('class', 'dot')
-					.attr('fill', function() { return "steelblue"; })
-					.attr('cx', function(d) { return x(d["date"]); })
-					.attr('cy', function(d) { return y(d["close"]); })
-					.attr('r', function() { return 3; })
-					.on("mouseover", function(d) {
-		  				d3.select(this)
-							.attr("r", 8)
-							.attr("class", "dot-selected")
-							.transition()
-		      					.duration(750);
-					})
-					.on("mouseout", function(d) {
-		  				d3.select(this)
-							.attr("r", 3)
-							.attr("class", "dot")
-							.transition()
-		      					.duration(750);
-					});
-	 
-	});
 }
 
 d3.select(self.frameElement).style("height", height + "px");
