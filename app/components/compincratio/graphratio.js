b3cc1e35553928a074539e6ecbf2cd1c6b03d7a8
Ricardo Alanis <ricardo.alanis@gmail.com>
2015-02-03 19:08:41 -0600

    Added new ratio graph using real data

--- app/components/compincratio/graphratio.js
+++ app/components/compincratio/graphratio.js
@@ -1,64 +1,92 @@
-function graphratio(datasource,ciudad) {
+function graphratio(data,ciudad) {
 	// Set the dimensions of the canvas / graph
-	var	margin = {top: 75, right: 50, bottom: 75, left: 50},
-		width = 600 - margin.left - margin.right,
-		height = 135 - margin.top - margin.bottom;
+
+	 d3.csv(data, function(error, csv) {
+
+		var year;
+		var ratio;
+        var ratioData;
+        var ratioData2;
+        var ratioDataarr = [];
+        var valueratio;
+        var ratio;
+
+        // Parse the date / time
+        var parseDate = d3.time.format("%Y").parse;
+
+		//Empieza Sección Nacional
+        
+        csv = csv.filter(function(row) {
+            return row['Tipo GINI'] == "Urbano";
+        });
+
+		csv= csv.filter(function(row) {
+        	return row['Ciudad'] == ciudad;
+    	});
+
+
+		//Aqui termina codigo nacional
+
+        var csvlen= csv.length;
+        for (var i = 0; i < csvlen; i++) {
+            year = parseDate(csv[i]['Ano']);
+            ratio = +csv[i]['RazonD1-D10'];
+            intratio= parseInt(ratio)
+            if(ratio) {
+                ratioData = { value: intratio, year:year, color:"Red" }
+                ratiofinal = intratio
+                }
+            }
+
+    ratioData2={ value: 1, year:year, color:"blue" }
+    ratioDataarr.push(ratioData2)
+    ratioDataarr.push(ratioData)
+
+
+
+	var	margin = {top: 25, right: 25, bottom: 25, left: 25},
+		width = 250 - margin.left - margin.right,
+		height = 250 - margin.top - margin.bottom;
 	 
 
 	var transbolita 
 
 
 	// Adds the svg canvas
-	var	svg = d3.select("chart4a")
+	var	svg = d3.select("div.chart4a")
 		.append("svg")
 			.attr("width", width + margin.left + margin.right)
 			.attr("height", height + margin.top + margin.bottom)
 		.append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	// Get the data
-	d3.csv(datasource, function(error, data) {
-		data.forEach(function(d) {
-			d.value = +d.value;
-			d.color = d.color;
-		};
-
-		csv = csv.filter(function(row) {
-	        return row['Ciudad'] == ciudad;}););
-
-	       var csvlen= csv.length;
-        for (var i = 0; i < csvlen; i++) {
-            anho = parseDate(csv[i]['anho']);
-            ratio = +csv[i]['ingreso_total_salarios'];
-
-	var max = d3.max(data, function(d) { return +d.value;} );
+       
+	var max = d3.max(ratioDataarr, function(d) { return +d.value;} );
 
 	var circles = svg.selectAll("circle")
-	                          .data(data)
+	                          .data(ratioDataarr)
 	                          .enter()
 	                          .append("circle");
 
 	var circleAttributes = circles
 			               .attr("cx", function(d, i) {
-			                   return i*width/6.0;
+			                   return i*width/3.0;
 			               })
-			               .attr("cy", height / 2)
-	                       .attr("r", function (d) { return d.value; })
+			               .attr("cy", height / 24)
+	                       .attr("r", function(d) { return d.value; })
 	                       .style("fill", function(d) { return d.color; });
 
 
 	var labels = svg.selectAll("text")
-	                          .data(data)
+	                          .data(ratioDataarr)
 	                          .enter()
 	                          .append("text");
 
 	 var circleAttributes = labels
-			               .attr("x", function(d, i) { return i*width/6.0-5-i*max/4; })
-			               .attr("y", function(d) { return height / 2 + max*2; })
-	                       .text(function (d) { return d.value; })
+			               .attr("x", function(d, i) { return i*width/3-9; })
+			               .attr("y", height/24+50)
+	                       .text(function(ratioDataarr) { return ratioDataarr.value; })
 	                       .attr("font-family", "sans-serif")
 							.attr("font-size", "20px")
 							.attr("fill", "black");                      
  
-});
-}
+   })}
